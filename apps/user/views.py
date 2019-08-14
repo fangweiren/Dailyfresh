0be87df48f429f34317926d6c047496ea746ab44
fangweiren <fwr843@163.com>
2019-08-14 16:24:35 +0800

    3-05_生成激活用户token

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views.generic import View
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from django.conf import settings
 from .models import User
 import re
 
@@ -11,6 +13,7 @@ class RegisterView(View):
     """
     注册
     """
+
     def get(self, request):
         """
         显示注册页面
@@ -49,5 +52,15 @@ class RegisterView(View):
         user = User.objects.create_user(username, email, password)
         user.is_active = 0
         user.save()
+
+        # 发送激活邮件，包含激活链接：http://127.0.0.1:8000/user/active/1
+        # 激活链接中需要包含用户的身份信息，并且要把身份信息进行加密
+        # 加密用户的身份信息，生成激活token
+        serializer = Serializer(settings.SECRET_KEY, 3600)
+        info = {"confirm": user.id}
+        token = serializer.dumps(info)
+
+        # 发邮件
+
 
         return redirect(reverse('goods:index'))
