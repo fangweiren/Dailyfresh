4db1031a6e1e603918e2210bfc700b066754ac22
fangweiren <fwr843@163.com>
2019-08-14 17:31:39 +0800

    3-06_注册用户激活流程

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
-from apps.user.views import RegisterView
+from django.urls import path, re_path
+from apps.user.views import RegisterView, ActiveView, LoginView
 
 app_name = 'user'
 urlpatterns = [
-    path('register', RegisterView.as_view(), name='register'),
+    path('register', RegisterView.as_view(), name='register'),  # 注册
+    path('login', LoginView.as_view(), name='login'),  # 登录
+    re_path('active/(?P<token>.*)', ActiveView.as_view(), name='active'),  # 用户激活
 ]
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views.generic import View
+from django.http import HttpResponse
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from itsdangerous import SignatureExpired
 from django.conf import settings
 from .models import User
 import re
@@ -34,7 +36,7 @@ class RegisterView(View):
             return render(request, 'register.html', {'errmsg': '数据不完整'})
 
         # 邮箱校验
-        if not re.match(r'^[a-z0-9][\w.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$', email):
+        if not re.match(r'^[a-z0-9][\w.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1, 2}$', email):
             return render(request, 'register.html', {'errmsg': '邮箱格式不正确'})
 
         if allow != 'on':
@@ -62,5 +64,37 @@ class RegisterView(View):
 
         # 发邮件
 
-
         return redirect(reverse('goods:index'))
+
+
+class ActiveView(View):
+    """用户激活"""
+
+    def get(self, request, token):
+        """进行用户激活"""
+        # 进行解密，获取要激活的用户信息
+        serializer = Serializer(settings.SECRET_KEY, 3600)
+        try:
+            info = serializer.loads(token)
+            # 获取待激活用户的id
+            user_id = info['confirm']
+
+            # 根据id获取用户信息
+            user = User.objects.get(id=user_id)
+            user.is_active = 1
+            user.save()
+
+            # 跳转到登录页面
+            return redirect(reverse('user:login'))
+        except SignatureExpired as e:
+            # 激活链接已过期
+            return HttpResponse('激活链接已过期')
+
+
+# /user/login
+class LoginView(View):
+    """登录"""
+
+    def get(self, request):
+        """显示登录页面"""
+        return render(request, 'login.html')
