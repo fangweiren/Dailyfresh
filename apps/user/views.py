1ae158aac5c81ab1e666a01097aa91de41a2212b
fangweiren <fwr843@163.com>
2019-08-19 16:35:13 +0800

    5-09_用户中心_地址页

--- apps/user/views.py
+++ apps/user/views.py
@@ -8,7 +8,7 @@ from itsdangerous import SignatureExpired
 from django.conf import settings
 from celery_tasks.tasks import send_register_active_email
 from utils.mixin import LoginRequiredMixin
-from .models import User
+from .models import User, Address
 import re
 
 
@@ -185,4 +185,57 @@ class AddressView(LoginRequiredMixin, View):
 
     def get(self, request):
         """显示"""
-        return render(request, 'user_center_site.html', {'page': 'address'})
+        # 获取登录用户对应User对象
+        user = request.user
+
+        # 获取用户的默认收货地址
+        try:
+            address = Address.objects.get(user=user, is_default=True)
+        except Address.DoesNotExist:
+            address = None
+
+        # 使用模板
+        return render(request, 'user_center_site.html', {'page': 'address', 'address': address})
+
+    def post(self, request):
+        """地址的添加"""
+        # 接收数据
+        receiver = request.POST.get('receiver')
+        addr = request.POST.get('addr')
+        zip_code = request.POST.get('zip_code')
+        phone = request.POST.get('phone')
+
+        # 校验数据
+        if not all([receiver, addr, phone]):
+            return render(request, 'user_center_site.html', {'errmsg': '数据不完整'})
+
+        # 校验手机号
+        if not re.match(r'^1[3|4|5|7|8][0-9]{9}$', phone):
+            return render(request, 'user_center_site.html', {'errmsg': '手机号不正确'})
+
+        # 业务处理：地址添加
+        # 如果用户已存在默认收货地址，添加的地址不作为默认收货地址，否则作为默认收货地址
+        # 获取登录用户对应User对象
+        user = request.user
+        try:
+            address = Address.objects.get(user=user, is_default=True)
+        except Address.DoesNotExist:
+            address = None
+
+        if address:
+            is_default = False
+        else:
+            is_default = True
+
+        # 添加地址
+        Address.objects.create(user=user,
+                               receiver=receiver,
+                               addr=addr,
+                               zip_code=zip_code,
+                               phone=phone,
+                               is_default=is_default)
+
+        # 返回应答 刷新地址页面
+        return redirect(reverse('user:address'))
+
+
