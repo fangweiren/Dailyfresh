149c9961c349e8a492bc74f3b7422a516cea91a3
fangweiren <fwr843@163.com>
2019-08-22 22:36:12 +0800

    7-08_商品模型模型类概述

--- apps/goods/views.py
+++ apps/goods/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import View
+
 
 # Create your views here.
-def index(request):
-    """
-    首页
-    """
-    return render(request, 'index.html')
+class IndexView(View):
+    """首页"""
+
+    def get(self, request):
+        """显示首页"""
+        return render(request, 'index.html')
