61701684c99570f5513b005ed2c1a4b15ee3e94d
fangweiren <fwr843@163.com>
2019-08-29 11:58:23 +0800

    7-01_页面静态化_celery生成首页静态页面

--- celery_tasks/tasks.py
+++ celery_tasks/tasks.py
@@ -2,6 +2,7 @@
 from celery import Celery
 from django.conf import settings
 from django.core.mail import send_mail
+from django.template import loader
 import time
 import os
 import django
@@ -9,6 +10,8 @@ import django
 # django环境初始化(在任务处理者一端加)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dailyfresh.settings")
 django.setup()
+
+from apps.goods.models import GoodsType, IndexGoodsBanner, IndexPromotionBanner, IndexTypeGoodsBanner
 
 # 创建celery类的实例对象
 app = Celery('celery_tasks.tasks', broker='redis://192.168.37.133:6379/8')
@@ -26,3 +29,43 @@ def send_register_active_email(to_email, username, token):
         username, token, token)
     send_mail(subject, message, sender, receiver, html_message=html_message)
     time.sleep(5)
+
+
+@app.task
+def generate_static_index_html():
+    """生成首页静态页面"""
+    # 获取商品的种类信息
+    types = GoodsType.objects.all()
+
+    # 获取首页轮播商品信息
+    goods_banners = IndexGoodsBanner.objects.all().order_by('index')
+
+    # 获取首页促销活动信息
+    promotion_banners = IndexPromotionBanner.objects.all().order_by('index')
+
+    # 获取首页分类商品展示信息
+    for type in types:
+        # 获取type种类首页分类商品的图片展示信息
+        image_banners = IndexTypeGoodsBanner.objects.filter(type=type, display_type=1).order_by('index')
+        # 获取type种类首页分类商品的文字展示信息
+        title_banners = IndexTypeGoodsBanner.objects.filter(type=type, display_type=0).order_by('index')
+
+        type.image_banners = image_banners
+        type.title_banners = title_banners
+
+    context = {
+        'types': types,
+        'goods_banners': goods_banners,
+        'promotion_banners': promotion_banners
+    }
+
+    # 使用模板
+    # 1.加载模板文件，返回模板对象
+    temp = loader.get_template('static_index.html')
+    # 2.模板渲染
+    static_index_html = temp.render(context)
+
+    # 生成首页对应静态文件
+    save_path = os.path.join(settings.BASE_DIR, 'static/index.html')
+    with open(save_path, 'w') as f:
+        f.write(static_index_html)
