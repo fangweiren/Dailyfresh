8b43ffaeb0ff27f9319a20eee84ad0362d7653fb
fangweiren <fwr843@163.com>
2019-08-29 16:56:47 +0800

    7-06_首页缓存数据的更新

--- apps/goods/admin.py
+++ apps/goods/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.core.cache import cache
 from .models import GoodsType, IndexGoodsBanner, IndexPromotionBanner, IndexTypeGoodsBanner
 
 
@@ -12,6 +13,9 @@ class BaseModelAdmin(admin.ModelAdmin):
         from celery_tasks.tasks import generate_static_index_html
         generate_static_index_html.delay()
 
+        # 清除首页的缓存数据
+        cache.delete('index_page_data')
+
     def delete_model(self, request, obj):
         """删除表中的数据时调用"""
         super().delete_model(request, obj)
@@ -19,6 +23,9 @@ class BaseModelAdmin(admin.ModelAdmin):
         # 发出任务，让celery worker重新生成首页静态页
         from celery_tasks.tasks import generate_static_index_html
         generate_static_index_html.delay()
+
+        # 清除首页的缓存数据
+        cache.delete('index_page_data')
 
 
 class GoodsTypeAdmin(BaseModelAdmin):
