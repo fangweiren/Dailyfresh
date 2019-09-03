fb8a7fe957ffa8c54ab05b754330832a699b100a
fangweiren <fwr843@163.com>
2019-09-03 22:29:49 +0800

    10-04_商品搜索_全文检索的使用

--- dailyfresh/urls.py
+++ dailyfresh/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tinymce/', include('tinymce.urls')),  # 富文本编辑器
+    path('search', include('haystack.urls')),  # 全文检索框架
     path('user/', include(('user.urls', "user"), namespace='user')),
     path('cart/', include(('cart.urls', "cart"), namespace='cart')),
     path('order/', include(('order.urls', "order"), namespace='order')),
