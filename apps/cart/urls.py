bba093d6734d20adff9d08385604981b6fe83727
fangweiren <fwr843@163.com>
2019-09-06 16:37:01 +0800

    12-02_购物车记录更新_后台view

--- apps/cart/urls.py
+++ apps/cart/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CartAddView, CartInfoView
+from .views import CartAddView, CartInfoView, CartUpdateView
 
 urlpatterns = [
     path('add', CartAddView.as_view(), name='add'),  # 购物车记录添加
+    path('update', CartUpdateView.as_view(), name='update'),  # 购物车记录更新
     path('', CartInfoView.as_view(), name='show'),  # 购物车页面显示
 ]
--- apps/cart/views.py
+++ apps/cart/views.py
@@ -104,3 +104,50 @@ class CartInfoView(LoginRequiredMixin, View):
             'total_price': total_price,
         }
         return render(request, 'cart.html', context)
+
+
+# /cart/update
+class CartUpdateView(View):
+    """购物车记录更新"""
+
+    def post(self, request):
+        """购物车记录更新"""
+        user = request.user
+        if not user.is_authenticated:
+            return JsonResponse({'res': 0, 'errmsg': '请先登录'})
+
+        # 接收数据
+        sku_id = request.POST.get('sku_id')
+        count = request.POST.get('count')
+
+        # 数据校验
+        if not all([sku_id, count]):
+            return JsonResponse({'res': 1, 'errmsg': '数据不完整'})
+
+        # 校验添加的商品数量
+        try:
+            count = int(count)
+        except Exception as e:
+            # 数目出错
+            return JsonResponse({'res': 2, 'errmsg': '商品数目出错'})
+
+        # 校验商品是否存在
+        try:
+            sku = GoodsSKU.objects.get(id=sku_id)
+        except GoodsSKU.DoesNotExist:
+            # 商品不存在
+            return JsonResponse({'res': 3, 'errmsg': '商品不存在'})
+
+        # 业务处理：更新购物车记录
+        conn = get_redis_connection('default')
+        cart_key = 'cart_%d' % user.id
+
+        # 校验商品的库存
+        if count > sku.stock:
+            return JsonResponse({'res': 4, 'errmsg': '商品库存不足'})
+
+        # 更新
+        conn.hset(cart_key, sku_id, count)
+
+        # 返回应答
+        return JsonResponse({'res': 5, 'message': '购物车更新成功'})
