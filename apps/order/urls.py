e9ae936b9c1e70a8491f57e144837c0093318fa8
fangweiren <fwr843@163.com>
2019-09-09 16:01:01 +0800

    13-03_提交订单页面显示

--- apps/order/urls.py
+++ apps/order/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from .views import OrderPlaceView
 
 urlpatterns = [
-
+    path('place', OrderPlaceView.as_view(), name='place'),  # 提交订单页面显示
 ]
--- apps/order/views.py
+++ apps/order/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views.generic import View
+from django_redis import get_redis_connection
+from apps.goods.models import GoodsSKU
+from apps.user.models import Address
+from utils.mixin import LoginRequiredMixin
 
-# Create your views here.
+
+# /order/place
+class OrderPlaceView(LoginRequiredMixin, View):
+    """提交订单页面显示"""
+
+    def post(self, request):
+        """提交订单页面显示"""
+        user = request.user
+
+        # 获取参数
+        sku_ids = request.POST.getlist('sku_ids')
+
+        if not sku_ids:
+            return redirect(reverse('cart:show'))
+
+        conn = get_redis_connection('default')
+        cart_key = 'cart_%d' % user.id
+
+        skus = []
+        total_count = 0
+        total_price = 0
+
+        for sku_id in sku_ids:
+            sku = GoodsSKU.objects.get(id=sku_id)
+            count = conn.hget(cart_key, sku_id)
+            amount = sku.price * int(count)
+            sku.count = int(count)
+            sku.amount = amount
+            skus.append(sku)
+            total_count += int(count)
+            total_price += amount
+
+        # 运费：实际开发时，属于一个子系统
+        # 这里写死
+        transit_price = 10
+
+        # 实付款
+        total_pay = total_price + transit_price
+
+        # 获取用户的收件地址
+        addrs = Address.objects.filter(user=user)
+
+        context = {
+            'skus': skus,
+            'total_count': total_count,
+            'total_price': total_price,
+            'transit_price': transit_price,
+            'total_pay': total_pay,
+            'addrs': addrs
+        }
+        return render(request, 'place_order.html', context)
